180be044a705f20d20188626689871f739631028
Paloftzer <phille04@outlook.com>
2026-03-27 12:49:08 +0100

    Added simple product rendering

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Frontend loaded');
 });
+
+import { loadAndRenderProducts } from './src/api.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadAndRenderProducts();
+});
