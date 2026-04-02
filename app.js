ee5a85d0c1f20d108765ff435c9c137321f335cb
Paloftzer <phille04@outlook.com>
2026-04-02 16:28:17 +0200

    Moved navbar code from template.html to nav.js to separate javascript from .html pages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+import { initNav } from './src/nav.js';
 import { loadAndRenderProducts } from './src/api.js';
 
 export function initApp() {
+  initNav();
   console.log('Frontend Loaded');
   
   if (document.getElementById('products')) {
